9b382f2597f5b857dc2f24b9af04d33ef186df64
wakr <wakr@cs.helsinki.fi>
2016-09-13 11:01:31 +0300

    Add a automatic test coverage hook

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,2 +1,2 @@
 pytest --cov-report term-missing --cov=experiment_server experiment_server/tests.py
-
+CODECLIMATE_REPO_TOKEN=81f7d3802f847de8d5d471351443281ce0dd7a6fd35e0a5141869b33417f9c8e codeclimate-test-reporter --file .coverage
