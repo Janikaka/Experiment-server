879946c21c0379ff4e89ad8a8593cca9e75790ac
Satu Kärkkäinen <satu.karkkainen@helsinki.fi>
2016-09-27 12:42:40 +0300

    Refactor views/applications

--- experiment_server/views/applications.py
+++ experiment_server/views/applications.py
@@ -1,11 +1,12 @@
+""" imports """
 from pyramid.view import view_config, view_defaults
-from pyramid.response import Response
-from ..models import DatabaseInterface
-import datetime
-from experiment_server.utils.log import print_log
-from .webutils import WebUtils
+# from pyramid.response import Response
 from experiment_server.models.applications import Application
 import sqlalchemy.orm.exc
+from experiment_server.utils.log import print_log
+from ..models import DatabaseInterface
+from .webutils import WebUtils
+
 
 @view_defaults(renderer='json')
 class Applications(WebUtils):
@@ -36,7 +37,7 @@ class Applications(WebUtils):
             })
 
         result = {'data': application.as_dict()}
-        print_log(name,'POST','/applications', 'Create new application', result)
+        print_log(name, 'POST', '/applications', 'Create new application', result)
         return self.createResponse(result, 200)
 
     @view_config(route_name='application', request_method="DELETE")
@@ -44,17 +45,16 @@ class Applications(WebUtils):
         """ Find and delete one application by id with destroy method """
         app_id = int(self.request.matchdict['id'])
         try:
-            if(Application.destroy(Application.get(app_id)) == None):
+            if Application.destroy(Application.get(app_id)) is None:
                 return "Delete completed."
-        except (sqlalchemy.orm.exc.UnmappedInstanceError):
-            pass
+        except sqlalchemy.orm.exc.UnmappedInstanceError:
             return "Delete failed."
 
     @view_config(route_name='configurationkeys_for_app', request_method="GET")
     def configurationkeys_for_application_GET(self):
         """ List all configurationkeys of specific application """
-        id = int(self.request.matchdict['id'])
-        return list(map(lambda _: _.as_dict(), Application.get(id).configurationkeys))
+        app_id = int(self.request.matchdict['id'])
+        return list(map(lambda _: _.as_dict(), Application.get(app_id).configurationkeys))
 
     @view_config(route_name='app_data', request_method="GET")
     def data_for_app_GET(self):
@@ -62,26 +62,26 @@ class Applications(WebUtils):
             Returns application with configurationkeys, rangeconstraints of conf.keys,
             operator of rangeconstraints
         """
-        id = int(self.request.matchdict['id'])
-        application = Application.get(id)
-        if application is None:
+        app_id = int(self.request.matchdict['id'])
+        app = Application.get(app_id)
+        if app is None:
             return self.createResponse(None, 400)
-        appAsJSON = application.as_dict()
+        app_json = app.as_dict()
         configurationkeys = []
-        for i in range(len(application.configurationkeys)):
-            ckey = application.configurationkeys[i]
-            ckeyAsJSON = ckey.as_dict()
+        for i in range(len(app.configurationkeys)):
+            ckey = app.configurationkeys[i]
+            ckey_json = ckey.as_dict()
             rconstraints = ckey.rangeconstraints
             rangeconstraints = []
-            for i in range(len(rconstraints)):
-                rconstraint = rconstraints[i]
-                rconstraintAsJSON = rconstraint.as_dict()
+            for r in range(len(rconstraints)):
+                rconstraint = rconstraints[r]
+                rconstraint_json = rconstraint.as_dict()
                 operator = rconstraint.operator
-                operatorAsJSON = operator.as_dict()
-                rconstraintAsJSON['operator'] = operatorAsJSON
-                rangeconstraints.append(rconstraintAsJSON)
-            ckeyAsJSON['rangeconstraints'] = rangeconstraints
-            configurationkeys.append(ckeyAsJSON)
-        appAsJSON['configurationkeys'] = configurationkeys
-        result = {'application': appAsJSON}
+                operator_json = operator.as_dict()
+                rconstraint_json['operator'] = operator_json
+                rangeconstraints.append(rconstraint_json)
+            ckey_json['rangeconstraints'] = rangeconstraints
+            configurationkeys.append(ckey_json)
+        app_json['configurationkeys'] = configurationkeys
+        result = {'application': app_json}
         return self.createResponse(result, 200)
