fc941ad3b6cd54cb758c93d08043f02b795b1ac1
wakr <wakr@cs.helsinki.fi>
2016-10-10 13:22:53 +0300

    Add CORS headers to accept any external requests

--- experiment_server/__init__.py
+++ experiment_server/__init__.py
@@ -5,6 +5,19 @@ from sqlalchemy import engine_from_config
 
 import experiment_server.database.orm as orm_config
 
+def add_cors_headers_response_callback(event):
+    def cors_headers(request, response):
+        response.headers.update({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Max-Age': '1728000',
+        })
+    event.request.add_response_callback(cors_headers)
+
+
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
@@ -16,9 +29,12 @@ def main(global_config, **settings):
         orm.sessionmaker(extension=ZopeTransactionExtension(), autoflush=True))
     orm_config.DBSession.configure(bind=engine)
 
+    from pyramid.events import NewRequest
+    config.add_subscriber(add_cors_headers_response_callback, NewRequest)
 
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
     config.scan()
+
     return config.make_wsgi_app()
