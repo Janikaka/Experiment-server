209c59e7425c2abc8472d043884ae9ec22f107f5
Matti Räty <matti.raty@helsinki.fi>
2016-11-09 14:46:41 +0200

    trying to edit configuration file

--- runapp.py
+++ runapp.py
@@ -6,11 +6,12 @@ from waitress import serve
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app = loadapp('config:production.ini', relative_to='.')
 
     parser = ConfigParser.SafeConfigParser()
     parser.read('production.ini')
     parser.set('app:main', 'sqlalchemy.url', os.environ['DATABASE_URL'])
     parser.write('production.ini')
+
+    app = loadapp('config:production.ini', relative_to='.')
 
     serve(app, host='0.0.0.0', port=port)
