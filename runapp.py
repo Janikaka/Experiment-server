53a59b52216d1f763247d8ca6eaac8b320a99443
Matti Räty <matti.raty@helsinki.fi>
2016-11-09 14:18:58 +0200

    moving configurations from productions-file for testing

--- runapp.py
+++ runapp.py
@@ -6,5 +6,17 @@ from waitress import serve
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:production.ini', relative_to='.')
-        
-    serve(app, host='0.0.0.0', port=port)
+
+    settings = {}
+    settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
+    settings['pyramid.includes'] = ['pyramid_tm']
+    settings['pyramid.reload_templates'] = false
+    settings['pyramid.debug_authorization'] = false
+    settings['pyramid.debug_notfound'] = false
+    settings['pyramid.debug_routematch'] = false
+    settings['pyramid.default_locale_name'] = en
+    settings['pyramid.includes'] = pyramid_swagger
+
+    pyramid_swagger.use_models = True
+
+    serve(experiment_server.main({}, **settings), host='0.0.0.0', port=port)    
