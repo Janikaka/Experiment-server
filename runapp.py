61edea5488f64d68aada004660bdec18ae49dc15
Matti Räty <matti.raty@helsinki.fi>
2016-11-09 14:55:16 +0200

    added check if enviroment has DATABASE_URL

--- runapp.py
+++ runapp.py
@@ -7,11 +7,14 @@ from waitress import serve
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
-    parser = configparser.SafeConfigParser()
-    parser.read('production.ini')
-    parser.set('app:main', 'sqlalchemy.url', os.environ['DATABASE_URL'])
-    with open('production.ini', 'w') as configfile:
-        parser.write(configfile)
+    db_url = os.environ['DATABASE_URL']
+
+    if db_url != None:
+        parser = configparser.SafeConfigParser()
+        parser.read('production.ini')
+        parser.set('app:main', 'sqlalchemy.url', db_url)
+        with open('production.ini', 'w') as configfile:
+            parser.write(configfile)
 
     app = loadapp('config:production.ini', relative_to='.')
 
