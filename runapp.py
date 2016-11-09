ec83ce9d66e2891c51c8016d22d1d84746591131
Matti Räty <matti.raty@helsinki.fi>
2016-11-09 14:45:52 +0200

    trying to edit configuration file

--- runapp.py
+++ runapp.py
@@ -1,6 +1,6 @@
+import ConfigParser
 import os
 
-from configparser import SafeConfigParser
 from paste.deploy import loadapp
 from waitress import serve
 
@@ -8,12 +8,9 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:production.ini', relative_to='.')
 
-    db_address = os.environ['DATABASE_URL']
-
-    if db_address != None:
-        print("***\n" + db_address + "\***")
-        parser = SafeConfigParser()
-        parser.read('production.ini')
-        parser.set('app:main', 'sqlalchemy.url', db_address)
+    parser = ConfigParser.SafeConfigParser()
+    parser.read('production.ini')
+    parser.set('app:main', 'sqlalchemy.url', os.environ['DATABASE_URL'])
+    parser.write('production.ini')
 
     serve(app, host='0.0.0.0', port=port)
