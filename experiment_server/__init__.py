e72c8a2fe7dfaf83780aab4e953bf5438028b3ec
Matti Räty <matti.raty@helsinki.fi>
2016-11-16 15:53:40 +0200

    inspecting routes

--- experiment_server/__init__.py
+++ experiment_server/__init__.py
@@ -42,6 +42,6 @@ def main(global_config, **settings):
     config.include('.routes')
     config.scan()
 
-    settings['pyramid_swagger.enable_path_validation'] = True
+    settings['pyramid_swagger.enable_path_validation'] = False
 
     return config.make_wsgi_app()
